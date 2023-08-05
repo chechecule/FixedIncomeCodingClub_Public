5947324073dcab0ef4721d0dfc234bb1bd977189
chechecule <115871358+chechecule@users.noreply.github.com>
2023-08-05 20:42:57 +0900

    new start4

---
+++ Data_Retrievers/src/utils/fsctest.py
@@ -0,0 +1,46 @@
+# FSC/wrappers.py
+import xmltodict
+import datetime
+import pandas as pd
+from functools import reduce
+
+from core.validators import isolate_params
+from core.wrapper_decorators import required_kwargs, recommended_kwargs
+from core.env_functions import get_dotenv, get_env_variable, get_env_variable_list
+
+from FSC.API import FSCPaginatorAPI
+
+get_dotenv("FSC.env")
+FSC_key = get_env_variable("Key")
+
+fsc = FSCPaginatorAPI(
+    APIKey = FSC_key
+)
+
+fsc.set_path("/1160100/service/GetBondIssuInfoService/getBondBasiInfo")
+fsc.set_return_type("xml")
+fsc.skip_validation = True
+
+kwargs = {"numOfRows" : 2, "pageNo" : 1, "basDt" : 20200409} 
+
+non_iterator_params = isolate_params(
+    needed_kwargs = ["basDt", "numOfRows", "pageNo"],
+    kwargs = kwargs
+)
+
+print(fsc)
+
+fsc.initialize_params_for_iterator(**non_iterator_params)
+fsc._set_paginator_params()
+
+bond_data_list = list()
+
+for (ret, r) in fsc:
+    bond_data_list.append(r["response"]["body"]["items"]["item"])
+
+    break
+
+#pd.DataFRame(bond_data_list)
+#print(r["response"]["body"]["items"]["item"])
+
+#print(pd.DataFrame(r["response"]["body"]["items"]["item"]))
