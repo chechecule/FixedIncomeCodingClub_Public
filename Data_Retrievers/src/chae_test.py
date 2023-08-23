a87c51d64b705d34f5d4f5c023d471bba06f8acc
chechecule <115871358+chechecule@users.noreply.github.com>
2023-08-23 20:02:14 +0900

    20230823 2차 커3

--- Data_Retrievers/src/chae_test.py
+++ Data_Retrievers/src/chae_test.py
@@ -1,14 +1,29 @@
 from API.FSC import wrappers
+from datetime import datetime, timedelta
+# date time import
 
-#wrappers를 가져옴.
+date = 1 # 이건 설정 / Day 설정
+current_date = datetime.now()
+#일자설정
 
-a = wrappers.getBondData(basDt="20230803")
-#데이터 가져오기
+for i in range(date):
+    t = current_date - timedelta(days=i+1)
+    Dt = t.strftime("%Y%m%d")
+    data = wrappers.getBondData(basDt=Dt)
+    selected_data = data[["basDt", "crno"]]  # "basDt"와 "crno" 열 선택
+    print(selected_data)
 
-a.columns
-print(a)
-a[["basDt", "scrsItmsKcdNm", "bondIsurNm", "bondSrfcInrt"]]
-d
+#일자 바꿔가면서 하려고
+
+
+
+
+print(data.columns)
+
+#a.columns
+#print(a)
+#a[["basDt", "scrsItmsKcdNm", "bondIsurNm", "bondSrfcInrt"]]
+
 '''
 import psycopg2
 
